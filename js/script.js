e2e3af166dc73a9f6d2bddc20ffb08bef2ab36b5
micha de haan <130181637+MichadeHaan@users.noreply.github.com>
2025-03-22 15:38:09 +0100

    update voor goede demo

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const loginLink = document.getElementById('login-link');
 
 if (localStorage.getItem('loggedIn') === 'true') {
     loginLink.textContent = 'Log uit';
-    loginLink.href = '../html/shop.html';
+    loginLink.href = '/index.html';
     loginLink.addEventListener('click', () => {
         localStorage.setItem('loggedIn', 'false');
     });
